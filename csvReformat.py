ce25ac21d5dc9e32170fa711c1cbaaf7c05517e0
taanish <arora.taanish@gmail.com>
2023-06-02 23:58:44 +0530

    added timestamp processor, finished reformatter

--- csvReformat.py
+++ csvReformat.py
@@ -1,23 +1,53 @@
 import csv
-def reformatCSV (Attendance.csv):
-    data = {}
-    with open (Attendance.csv, 'r') as f:
-        reader = csv.reader(f)
+import json
+def timeStampProcessor(ts):
+    date, time = ts.split(' ', 1)  #Seperates date and time from timeStamp variable
+    half = time[-2:] #PM or AM
+    time = time[:-2] #Time of day in 12Hr format
+
+    hour, min, sec = time.split(':') #Seperates time variable into hour, min, and second
+
+    hour, min, sec = int(hour), int(min), int(sec) 
+
+    if half == 'PM': #Converts initialHour from 12Hr format to 24Hr format
+        if hour < 12:
+            hour = hour + 12 
+
+    tempTime = (hour*60) + min
+    return tempTime
+
+
+
+def reformatCSV (Attendance):
+    actionData = {}
+    timeData = {}
+    with open (Attendance, 'r', encoding= 'utf-16') as f:
+        reader = csv.reader(f, delimiter = '\t')
+
+        next(reader)
+
+        for row in reader:
+            name= row[0]
+            action= row[1]
+            timeStamp= timeStampProcessor(row[2])
+
+            if name in actionData:
+                actionData[name].append(action)
+                timeData[name].append(timeStamp)
+            else:
+                actionData[name]= [action]
+                timeData[name]= [timeStamp]
+
+    action = [{name: actions} for name, actions in actionData.items()]
+    time= [{name: timeStamp} for name, timeStamp in timeData.items()]
     
-    next(reader)
+    
+    return action, time
+ 
+tempAction , tempTime = reformatCSV("demoAttendance.csv")
 
-    for row in reader:
-        name= row[0]
-        action= row[1]
-        timeStamp= row[2]
-
-        if name in data:
-            data[name].append(action)
-        else:
-            data[name]= action
-
-    newData = [{name: actions} for name, actions in data.items()]
-    return newData
+print(json.dumps(tempAction, indent=4))
+print(json.dumps(tempTime, indent=4))
 
     
 
