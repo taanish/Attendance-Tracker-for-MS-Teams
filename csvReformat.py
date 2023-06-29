458b7c581dc7816a75c1cd6c360a33cc984e613c
taanish <arora.taanish@gmail.com>
2023-06-30 03:27:39 +0530

    Reformatted the output of attendanceReport from dict to list to allow for easy updation using google sheets API. Core attendance report generation is now completed.

--- csvReformat.py
+++ csvReformat.py
@@ -1,32 +1,41 @@
 import csv
 import json
 
-endTime = 800 #demo value for class endtime 
+# demo value for class startTime and endTime, to be inputted by the user during main()
+classStartTime = 760
+classEndTime = 800
+classDuration = classEndTime - classStartTime
+
+
+# Note: implement functtionality for processing class starttime and class endtime in timeStampProcessor
 
 def timeStampProcessor(ts):
-    date, time = ts.split(' ', 1)  #Seperates date and time from timeStamp variable
-    half = time[-2:] #PM or AM
-    time = time[:-2] #Time of day in 12Hr format
+    # Seperates date and time from timeStamp variable
+    date, time = ts.split(' ', 1)
+    half = time[-2:]  # PM or AM
+    time = time[:-2]  # Time of day in 12Hr format
 
-    hour, min, sec = time.split(':') #Seperates time variable into hour, min, and second
+    # Seperates time variable into hour, min, and second
+    hour, min, sec = time.split(':')
 
-    hour, min, sec = int(hour), int(min), int(sec) 
+    hour, min, sec = int(hour), int(min), int(sec)
 
-    if half == 'PM': #Converts initialHour from 12Hr format to 24Hr format
+    if half == 'PM':  # Converts initialHour from 12Hr format to 24Hr format
         if hour < 12:
-            hour = hour + 12 
+            hour = hour + 12
 
     tempTime = (hour*60) + min
     return tempTime
 
 
-def addEndTime (timeList, endTime):  #A) Ensures that every element in timeList is even    B) Every item in timeList ends with the time they left the class
+# A) Ensures that every list in timeList has even number of items    B) Every item in timeList ends with the time they left the class
+def addEndTime(timeList, endTime):
     for i in timeList:
-        l= len(i)
+        l = len(i)
 
-        if (l%2==1):
+        if (l % 2 == 1):
             i.append(endTime)
-    
+
     return timeList
 
 
@@ -35,61 +44,41 @@ def removeDuplicateNames(nameList):
     for name in nameList:
         if name not in uniqueNames:
             uniqueNames.append(name)
-    
+
     return uniqueNames
 
 
-
-def reformatCSV (Attendance):
+def reformatCSV(Attendance):
     actionData = {}
     timeData = {}
-    nameList= [] 
-    timeList= [] 
-    with open (Attendance, 'r', encoding= 'utf-16') as f:
-        reader = csv.reader(f, delimiter = '\t')
+    nameList = []
+    timeList = []
+    with open(Attendance, 'r', encoding='utf-16') as f:
+        reader = csv.reader(f, delimiter='\t')
 
         next(reader)
 
         for row in reader:
-            name= row[0]
+            name = row[0]
             nameList.append(name)
-            action= row[1]
-            timeStamp= timeStampProcessor(row[2])
+            action = row[1]
+            timeStamp = timeStampProcessor(row[2])
 
             if name in actionData:
                 actionData[name].append(action)
                 timeData[name].append(timeStamp)
             else:
-                actionData[name]= [action]
-                timeData[name]= [timeStamp]
-    
-    
+                actionData[name] = [action]
+                timeData[name] = [timeStamp]
+
     actionNameDict = [{name: actions} for name, actions in actionData.items()]
-    timeNameDict= [{name: timeStamp} for name, timeStamp in timeData.items()]
-    
-    
+    timeNameDict = [{name: timeStamp} for name, timeStamp in timeData.items()]
+
     for dict in timeNameDict:
         for val in dict.values():
             timeList.append(val)
-    
-    timeList = addEndTime(timeList, endTime)
+
+    timeList = addEndTime(timeList, classEndTime)
     nameList = removeDuplicateNames(nameList)
 
-
     return timeList, nameList, actionNameDict, timeNameDict
-
-
-    
- 
-# timeList, nameList, actionNameDict , timeNameDict = reformatCSV("demoAttendance.csv")
-
-# print(timeList)
-# print(nameList)
-# print(json.dumps(actionNameDict))
-# print(json.dumps(timeNameDict))
-
-
-
-    
-
-
--- joinStatus.py
+++ joinStatus.py
@@ -3,32 +3,41 @@ import json
 from textwrap import indent
 import csvReformat
 
-timeList, nameList, actionNameDict , timeNameDict = csvReformat.reformatCSV('demoAttendance.csv')
+timeList, nameList, actionNameDict, timeNameDict = csvReformat.reformatCSV(
+    'demoAttendance.csv')
 
-def computeJoinTimeList (timeList):
-    joinTimeList= []
+
+# Takes raw timeList with all timestamps and parses it to extract total joinTimes and returns it via joinTimeList
+def computeJoinTimeList(timeList):
+    joinTimeList = []
     for i in timeList:
-        l= len(i)
-        joinTime= 0
-    
-        for j in range (1, l):
-            if (j%2==1):
-                joinTime+= i[j] - i[j-1]
-            
+        l = len(i)
+        joinTime = 0
+
+        for j in range(1, l):
+            if (j % 2 == 1):
+                joinTime += i[j] - i[j-1]
+
         joinTimeList.append(joinTime)
 
-        
-    tempNameTime = [{n:jTime} for n, jTime in zip(nameList,joinTimeList)]
-    
-    return tempNameTime
+    return joinTimeList
 
 
+# Combines nameList (unique) and joinTimeList to generate a final attendance report
+def attendanceStatus():
+    attendanceReport = []
+
+    joinTimeList = computeJoinTimeList(timeList)
+    timeThreshold = int(input("Out of the %s mins of class time, how many minutes should a student attend in order to get a present mark?" % (
+        csvReformat.classDuration)))
+
+    for i in range(len(joinTimeList)):
+        if (joinTimeList[i] >= timeThreshold):
+            attendanceReport.append([nameList[i], 'Yes'])
+        else:
+            attendanceReport.append([nameList[i], 'No'])
+
+    return attendanceReport
 
 
-
-
-    
-
-
-
-
+print(attendanceStatus())
